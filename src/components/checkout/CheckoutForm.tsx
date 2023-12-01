614fef6fadbcfdac0505ccf6c6a0e7a35d1d3340
Májai Henrietta - Zita <majai.henrietta@student.ms.sapientia.ro>
2023-12-01 19:27:03 +0200

    refactor: checkout page redesign

--- src/components/checkout/CheckoutForm.tsx
+++ src/components/checkout/CheckoutForm.tsx
@@ -8,34 +8,39 @@ const CheckoutForm = () => {
 	};
 
 	return (
-		<Container maxWidth="lg">
-			<Typography variant="h4" gutterBottom>
+		<Container maxWidth="lg" >
+			<Typography variant="h4" gutterBottom sx={{mb:4}}>
 				Checkout
 			</Typography>
-			<Grid container spacing={3}>
+			<Grid container spacing={3} >
 				<Grid item xs={12} md={7}>
-					<Box component="form" onSubmit={handleSubmit} noValidate>
-						<Typography variant="h6">Contact information</Typography>
-						<TextField required label="First name" fullWidth margin="normal" />
-						<TextField required label="Last name" fullWidth margin="normal" />
-						<TextField required label="Email address" fullWidth margin="normal" />
-
-						<Typography variant="h6" gutterBottom>
+					<Box component="form" onSubmit={handleSubmit} noValidate sx={{border: '1px solid #BDBFBD', borderRadius: '8px', boxShadow: 'none', p:2}}>
+						<Typography variant="h6" >
+							Contact information
+						</Typography>
+						<div style={{ display: 'flex', gap: '16px' }}>
+  							<TextField required label="First name" fullWidth margin="normal" sx={{ ".MuiInputBase-root": { borderColor: 'neutral.70', borderRadius: '8px' } }} />
+  							<TextField required label="Last name" fullWidth margin="normal" sx={{ ".MuiInputBase-root": { borderColor: 'neutral.70', borderRadius: '8px' } }} />
+						</div>
+						<TextField required label="Email address" fullWidth margin="normal" sx = {{ ".MuiInputBase-root": { borderColor: 'neutral.70', borderRadius:'8px'}}} />
+						<Typography variant="h6" gutterBottom  sx={{ mt:2}}>
 							Shipping address
 						</Typography>
-						<TextField required label="Street address" fullWidth margin="normal" />
-						<TextField required label="Country" fullWidth margin="normal" />
-						<TextField required label="Town / City" fullWidth margin="normal" />
-						<TextField label="State" fullWidth margin="normal" />
-						<TextField required label="Zip code" fullWidth margin="normal" />
+						<TextField required label="Street address" fullWidth margin="normal" sx = {{ ".MuiInputBase-root": { borderColor: 'neutral.70', borderRadius:'8px'}}} />
+						<TextField required label="Country" fullWidth margin="normal" sx = {{ ".MuiInputBase-root": { borderColor: 'neutral.70', borderRadius:'8px'}}} />
+						<TextField required label="Town / City" fullWidth margin="normal" sx = {{ ".MuiInputBase-root": { borderColor: 'neutral.70', borderRadius:'8px'}}} />
+						<TextField label="State" fullWidth margin="normal"  sx = {{ ".MuiInputBase-root": { borderColor: 'neutral.70', borderRadius:'8px'}}}/>
+						<TextField required label="Zip code" fullWidth margin="normal"sx = {{ ".MuiInputBase-root": { borderColor: 'neutral.70', borderRadius:'8px'}}} />
 
-						<Typography variant="h6" gutterBottom>
+						<Typography variant="h6" gutterBottom sx={{ mt:2}}>
 							Payment information
 						</Typography>
-						<TextField required label="Card number" fullWidth margin="normal" />
-						<TextField required label="Expiry date" fullWidth margin="normal" />
-						<TextField required label="CVC code" fullWidth margin="normal" />
-						<Button type="submit" fullWidth variant="contained"  sx={{mt: '8px', fontWeight: 'bold',fontSize: 20,border: '2px solid',borderColor: 'primary.main',borderRadius: '8px',py: 1,px: 7,width: '100%',color: 'white',mb: '24px',ml:'24px',mr:'24px'}}>
+						<TextField required label="Card number" fullWidth margin="normal" sx = {{ ".MuiInputBase-root": { borderColor: 'neutral.70', borderRadius:'8px'}}} />
+						<div style={{ display: 'flex', gap: '16px' }}>
+  							<TextField required label="Expiry date" fullWidth margin="normal" sx={{ ".MuiInputBase-root": { borderColor: 'neutral.70', borderRadius: '8px' } }}/>
+							<TextField required label="CVC code" fullWidth margin="normal" sx={{ ".MuiInputBase-root": { borderColor: 'neutral.70', borderRadius: '8px' } }}/>
+						</div>
+						<Button type="submit" fullWidth variant="contained"  sx={{mt: '8px', fontWeight: 'bold',fontSize: 20,border: '2px solid',borderColor: 'primary.main',borderRadius: '8px',width: '100%',color: 'white',mb: '24px'}}>
 							Place order
 						</Button>
 					</Box>
