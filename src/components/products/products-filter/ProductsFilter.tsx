e78d5fa23026469619a71afbccb1be60d5d75933
Becze Balazs <balazs7699@gmail.com>
2023-12-02 16:45:27 +0200

    feat: integrate filters logic

--- src/components/products/products-filter/ProductsFilter.tsx
+++ src/components/products/products-filter/ProductsFilter.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import React, { Dispatch, FC, SetStateAction } from 'react';
 import { v4 as uuid } from 'uuid';
 
 import Checkbox from '@mui/material/Checkbox/Checkbox';
@@ -8,35 +8,46 @@ import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 
 interface Props {
-	title: string;
-	options: Array<String>;
+    title: string;
+    options: string[];
+    setFilter?: Dispatch<SetStateAction<string[]>>;
+    selectedFilters?: string[];
 }
 
-const ProductsFilter: FC<Props> = props => {
-	const { title, options } = props;
+const ProductsFilter: FC<Props> = ({ title, options, setFilter, selectedFilters }) => {
+    const handleCheckboxChange = (item: string) => {
+        setFilter && setFilter(prev => {
+            if (prev.includes(item)) {
+                return prev.filter(i => i !== item);
+            } else {
+                return [...prev, item];
+            }
+        });
+    };
 
-	return (
-		<Paper elevation={8} sx={{ px: 2, py: 2 }}>
-			<Typography
-				sx={{
-					fontWeight: 600,
-					fontSize: '1.25rem',
-				}}
-			>
-				{title}
-			</Typography>
-			<FormGroup sx={{ display: 'flex', pt: 2 }}>
-				{options.map(item => (
-					<FormControlLabel
-						key={uuid()}
-						control={<Checkbox name={title} value={item} />}
-						label={item}
-					/>
-				))}
-			</FormGroup>
-		</Paper>
-	);
+    return (
+        <Paper elevation={8} sx={{ px: 2, py: 2 }}>
+            <Typography sx={{ fontWeight: 600, fontSize: '1.25rem' }}>
+                {title}
+            </Typography>
+            <FormGroup sx={{ display: 'flex', pt: 2 }}>
+                {options.map(item => (
+                    <FormControlLabel
+                        key={uuid()}
+                        control={
+                            <Checkbox
+                                name={title}
+                                value={item}
+                                onChange={() => handleCheckboxChange(item)}
+                                checked={selectedFilters?.includes(item)}
+                            />
+                        }
+                        label={item}
+                    />
+                ))}
+            </FormGroup>
+        </Paper>
+    );
 };
 
 export default ProductsFilter;
-
--- src/pages/phones/index.tsx
+++ src/pages/phones/index.tsx
@@ -1,6 +1,6 @@
 import React, { ChangeEvent, useState } from 'react';
 import MainLayout from '../../components/common/main-layout/MainLayout';
-import { Unstable_Grid2 as Grid, Stack } from '@mui/material';
+import { Unstable_Grid2 as Grid, Stack, Typography } from '@mui/material';
 import ProductCard from '../../components/products/product-card/ProductCard';
 import ProductsFilter from '../../components/products/products-filter/ProductsFilter';
 import useProductsQuery from '../../hooks/products/useProductsQuery';
@@ -9,6 +9,8 @@ import { FooterPagination } from '../../components/common/footer-pagination/Foot
 const PhonesPage = () => {
 	const phones = useProductsQuery('phones');
 	const [currentPage, setCurrentPage] = useState(1);
+	const [priceFilter, setPriceFilter] = useState<string[]>([]);
+	const [brandFilter, setBrandFilter] = useState<string[]>([]);
 
 	const itemsPerPage = 1;
 	const handlePageChange = (event: ChangeEvent<unknown>, value: number) => {
@@ -18,7 +20,24 @@ const PhonesPage = () => {
 			behavior: 'smooth',
 		});
 	};
-	const filteredPhones = phones.data?.slice(
+	const parsePriceRange = (range: string) => {
+		const [min, max] = range.split('-').map(Number);
+		return { min, max };
+	};
+
+	const isInPriceRange = (price: number, ranges: Array<{ min: number; max: number }>) => {
+		return ranges.some(range => price >= range.min && price <= range.max);
+	};
+
+	const priceRanges = priceFilter?.map(parsePriceRange);
+
+	const filteredPhones = phones.data?.filter((phone: any) => {
+		const priceMatch = priceRanges.length === 0 || isInPriceRange(phone.price, priceRanges);
+		const brandMatch = brandFilter.length === 0 || brandFilter.includes(phone.brand);
+		return priceMatch && brandMatch;
+	});
+
+	const paginatedPhones = filteredPhones?.slice(
 		(currentPage - 1) * itemsPerPage,
 		currentPage * itemsPerPage,
 	);
@@ -31,15 +50,24 @@ const PhonesPage = () => {
 						<ProductsFilter
 							title="Price"
 							options={['1-100', '100-200', '300-400', '400-500', '500-600', '600+']}
+							setFilter={setPriceFilter}
+							selectedFilters={priceFilter}
 						/>
 						<ProductsFilter
 							title="Brand"
 							options={['Samsung', 'Iphone', 'Xiaomi', 'Huawei', 'Google', 'LG']}
+							setFilter={setBrandFilter}
+							selectedFilters={brandFilter}
 						/>
 					</Stack>
 				</Grid>
 				<Grid container spacing={4} xs={10}>
-					{filteredPhones?.map((data: any) => (
+					{paginatedPhones?.length === 0 && (
+						<Stack alignItems={'center'} justifyContent={'center'} width={1 /1}>
+							<Typography variant="h5">No product found!</Typography>
+						</Stack>
+					)}
+					{paginatedPhones?.map((data: any) => (
 						<Grid xs={'auto'} key={data.id}>
 							<ProductCard
 								key={data.id}
@@ -51,14 +79,16 @@ const PhonesPage = () => {
 							/>
 						</Grid>
 					))}
-					<Grid xs={12} >
-						<FooterPagination
-							currentPage={currentPage}
-							handlePageChange={handlePageChange}
-							dataLength={phones.data?.length}
-							itemsPerPage={itemsPerPage}
-						/>
-					</Grid>
+					{paginatedPhones?.length !== 0 && (
+						<Grid xs={12}>
+							<FooterPagination
+								currentPage={currentPage}
+								handlePageChange={handlePageChange}
+								dataLength={filteredPhones?.length}
+								itemsPerPage={itemsPerPage}
+							/>
+						</Grid>
+					)}
 				</Grid>
 			</Grid>
 		</MainLayout>
