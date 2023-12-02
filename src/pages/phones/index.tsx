3fae4269beeb12a6f1b0c9a721c813b78b2ae6c9
Becze Balazs <balazs7699@gmail.com>
2023-12-02 15:48:55 +0200

    feat: create pagination

--- src/pages/phones/index.tsx
+++ src/pages/phones/index.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import MainLayout from '../../components/common/main-layout/MainLayout';
 import { Unstable_Grid2 as Grid, Stack } from '@mui/material';
 import ProductCard from '../../components/products/product-card/ProductCard';
 import ProductsFilter from '../../components/products/products-filter/ProductsFilter';
 import useProductsQuery from '../../hooks/products/useProductsQuery';
+import { FooterPagination } from '../../components/common/footer-pagination/FooterPagination';
 
 const PhonesPage = () => {
-    const phones = useProductsQuery('phones');
+	const phones = useProductsQuery('phones');
+	const [currentPage, setCurrentPage] = useState(1);
+
+	const itemsPerPage = 1;
+	const handlePageChange = (event: ChangeEvent<unknown>, value: number) => {
+		setCurrentPage(value);
+		window.scrollTo({
+			top: 0,
+			behavior: 'smooth',
+		});
+	};
+	const filteredPhones = phones.data?.slice(
+		(currentPage - 1) * itemsPerPage,
+		currentPage * itemsPerPage,
+	);
+
 	return (
 		<MainLayout>
 			<Grid container spacing={4}>
@@ -23,7 +39,7 @@ const PhonesPage = () => {
 					</Stack>
 				</Grid>
 				<Grid container spacing={4} xs={10}>
-					{phones.data?.map((data: any) => (
+					{filteredPhones?.map((data: any) => (
 						<Grid xs={'auto'} key={data.id}>
 							<ProductCard
 								key={data.id}
@@ -35,6 +51,14 @@ const PhonesPage = () => {
 							/>
 						</Grid>
 					))}
+					<Grid xs={12} >
+						<FooterPagination
+							currentPage={currentPage}
+							handlePageChange={handlePageChange}
+							dataLength={phones.data.length}
+							itemsPerPage={itemsPerPage}
+						/>
+					</Grid>
 				</Grid>
 			</Grid>
 		</MainLayout>
